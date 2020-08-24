3a9bf35f1b437f35ec7310710fdb461430036f5f
Avinash5284 <65229426+Avinash5284@users.noreply.github.com>
2020-08-24 22:46:22 +0530

    Add files via upload

---
+++ DAY6/DEMO2/1.js
@@ -0,0 +1,14 @@
+
+const PI =3.14;
+
+let sumFun=function (n1,n2)
+{
+   return n1+n2;
+
+}
+
+module.exports =  {
+
+    PI,
+    sumFun
+};
---
+++ DAY6/DEMO2/main.js
@@ -0,0 +1,15 @@
+
+
+const demo = require("./1");
+
+
+let total = demo.sumFun(10,20);
+
+console.log(total);
+
+const calculation=require("./2");
+
+let sum= calculation.sum(30,40);
+console.log(sum);
+
+console.log(calculation);
